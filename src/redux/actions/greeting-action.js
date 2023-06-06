0fc787325df275fef14a33363faa798199d692bb
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-06-06 11:13:09 +0100

    Add all components and redux

---
+++ src/redux/actions/greeting-action.js
@@ -0,0 +1,16 @@
+import { greetingActions } from "../slice/greetingSlice";
+
+export const fetchGreetingData = () => {
+  return async (dispatch) => {
+    const response = await fetch("http://localhost:3000/messages", {
+    });
+    const data = await response.json();
+
+    console.log(data);
+
+    data.forEach((el) => {
+      const greeting = el.text;
+      dispatch(greetingActions.addGreeting(greeting));
+    });
+  };
+};
